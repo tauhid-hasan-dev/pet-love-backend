9cc0982694ae5637caa9db32e4f55979f59a2a37
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-04-28 21:31:42 +0300

    error message updaetd

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
-import catchAsync from '../../../shared/catchAsync';
-import sendResponse from '../../../shared/sendResponse';
-import httpStatus from 'http-status';
-import { UserServices } from './user.services';
-
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { UserServices } from "./user.services";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserServices.createUser(req);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
     success: true,
-    message: 'User is created successfully!',
+    statusCode: httpStatus.CREATED,
+    message: "User registered successfully",
     data: result,
   });
 });
