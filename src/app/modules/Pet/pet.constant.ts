f829dd17948c6a64967b732e6baf83ae38b7ab81
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-26 12:10:28 +0300

    filterable fields updated

--- src/app/modules/Pet/pet.constant.ts
+++ src/app/modules/Pet/pet.constant.ts
@@ -1,5 +1,6 @@
 export const petFilterableFields = [
   "species",
+  "gender",
   "breed",
   "age",
   "size",
@@ -9,6 +10,7 @@ export const petFilterableFields = [
 
 export const petSearchableFields = [
   "name",
+  "gender",
   "species",
   "breed",
   "location",
