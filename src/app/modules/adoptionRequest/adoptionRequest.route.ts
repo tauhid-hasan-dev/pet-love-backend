4b19db6f081ce6f6943741175d0377160c074eb7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-30 00:14:21 +0300

    auth guard applied

--- src/app/modules/adoptionRequest/adoptionRequest.route.ts
+++ src/app/modules/adoptionRequest/adoptionRequest.route.ts
@@ -4,24 +4,25 @@ import validateRequest from "../../middlewares/validateRequest";
 import auth from "../../middlewares/auth";
 import { adoptionRequestValidationSchema } from "./adoptionRequest.validation";
 import { AdoptionRequestController } from "./adoptionRequest.controller";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 
 const router = express.Router();
 
 router.post(
   "/adoption-request",
-  auth(),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
   validateRequest(adoptionRequestValidationSchema.createAdoptionRequestSchema),
   AdoptionRequestController.createAdoptionRequest
 );
 
 router.get(
   "/adoption-requests",
-  auth(),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
   AdoptionRequestController.getAllFromDB
 );
 router.put(
   "/adoption-requests/:requestId",
-  auth(),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
   validateRequest(adoptionRequestValidationSchema.updateAdoptionRequestSchema),
   AdoptionRequestController.updateAdoptionRequest
 );
