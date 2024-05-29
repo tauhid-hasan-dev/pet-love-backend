f36e7e8ff99df7ffb8c9d2628e0c43bed946d4dc
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-30 00:31:04 +0300

    added cors origin and credentials

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import routes from './app/routes';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import httpStatus from 'http-status';
-import cookieParser from 'cookie-parser';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import routes from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import httpStatus from "http-status";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Pet love server is running......."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Pet love server is running.......",
+  });
 });
 
 //global error handler
@@ -28,16 +28,15 @@ app.use(globalErrorHandler);
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'Not Found',
+    message: "Not Found",
     errorMessages: [
       {
         path: req.originalUrl,
-        message: 'API Not Found',
+        message: "API Not Found",
       },
     ],
   });
   next();
 });
-
 
 export default app;
